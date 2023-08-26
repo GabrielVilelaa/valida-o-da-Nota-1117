f81cd252e2a646a859e23a942889234dbe8d2d5a
gabri <gabrielvilela.acer@gmail.com>
2023-08-25 22:21:30 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    class program {
+        static void Main(string[] args)
+        {
+            double grade1,grade2, sum;
+
+            grade1 = double.Parse(Console.ReadLine(), (CultureInfo.InvariantCulture));
+
+            sum = 0.0;
+            while (grade1 < 0.0 || grade1 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                grade1 = double.Parse(Console.ReadLine(), (CultureInfo.InvariantCulture));
+            }
+
+            grade2 = double.Parse(Console.ReadLine(), (CultureInfo.InvariantCulture));
+            while (grade2 > 0.0 || grade2 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                grade2 = double.Parse(Console.ReadLine(), (CultureInfo.InvariantCulture));
+
+            }
+            sum = (grade1 + grade2) / 2.0;
+            Console.WriteLine("media = " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
